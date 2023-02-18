5fab29cbed9ffdfc0914efafd52e178dcbc98975
ismaeldubuc <ismael.dubuc@gmail.com>
2023-02-18 21:44:58 +0100

    Ajout de l'édition et correction des bugs

--- script.js
+++ script.js
@@ -1,61 +1,101 @@
 var perso = document.querySelector("#personnage");
+
 var obstacles = document.querySelector("#obstacles");
 var obstacles2 = document.querySelector("#obstacles2");
 var obstacles3 = document.querySelector("#obstacles3");
 var obstacles4 = document.querySelector("#obstacles4");
+
+
 var accroupi = document.querySelector("#accroupi");
 //liste des obstacles
 var obstacleArray = [obstacles, obstacles2, obstacles3, obstacles4]
-
+var verification;
 //fonction pour déterminer quel obstacle va spawn
+var numeroObstacle;
+var dejaPasseParLa = false;
 
 function obstacleHandler(){
-    var randomNumber = Math.floor(Math.random() * obstacleArray.length);
-    console.log(randomNumber);
-    obstacleArray[randomNumber].style.display = "block";
-
+    var typeObstacle = 0;
+    numeroObstacle = 1;
+    typeObstacle = determinerTypeObstacle(numeroObstacle);
+    obstacleArray[typeObstacle].style.display = "block";
+    
     //verification s'il y a collision entre obstacles et perso
-    var verification = setInterval(function(){
-        var persoBottom = parseInt(window.getComputedStyle(perso).getPropertyValue("bottom")) ;
-        var persoAccroupi = parseInt(window.getComputedStyle(accroupi).getPropertyValue("bottom")) ;
-        var obstaclesLeft = parseInt(window.getComputedStyle(obstacleArray[randomNumber]).getPropertyValue("left"));
-        var obstaclesBottom = parseInt(window.getComputedStyle(obstacleArray[randomNumber]).getPropertyValue("bottom"));
+    verification = setInterval("detectionCollision()",1);
+}
+function detectionCollision(){
+    var persoBottom = parseInt(window.getComputedStyle(perso).getPropertyValue("bottom")) ;
+    var persoAccroupi = parseInt(window.getComputedStyle(accroupi).getPropertyValue("bottom")) ;
+    var obstaclesLeft = parseInt(window.getComputedStyle(obstacleArray[typeObstacle]).getPropertyValue("left"));
+    var obstaclesBottom = parseInt(window.getComputedStyle(obstacleArray[typeObstacle]).getPropertyValue("bottom"));
+    //console.log("persoBottom:"+persoBottom+"|persoAccroupi:"+persoAccroupi+"|obstaclesLeft:"+obstaclesLeft+"|obstaclesBottom:"+obstaclesBottom);
+    //console.log("obstaclesLeft:"+obstaclesLeft);
+    //console.log("Déjà passé par là : "+dejaPasseParLa);
 
-console.log(persoBottom)
+    if(obstaclesLeft<20 && obstaclesLeft >0 && (persoBottom< 114 && typeObstacle!=1 || (persoBottom>=50 && typeObstacle==1)) ){
 
-         if(obstaclesLeft<20 && obstaclesLeft >0 && (persoBottom< 114 && randomNumber!=1 || (persoBottom>=50 && randomNumber==1))){
+        afficherGameOver();
 
-            var titleOver = "Game Over";
+    } else if ((obstaclesLeft  < 0) && (dejaPasseParLa==false)) { // si l'obstacle n'est pas entré en collision avec le personnage
+        obstacleArray[typeObstacle].style.display = "none"; // on cache l'obstacle
+        dejaPasseParLa = true;
+        numeroObstacle++;
+        typeObstacle = determinerTypeObstacle(numeroObstacle); // on génère un nombre aléatoire
+        obstacleArray[typeObstacle].style.display = "block"; // nouvel obstacle selon le nombre aléatoire
+        //console.log("Obstacles Left:"+obstaclesLeft);
+        //console.log('Nombre obstacle franchi: ' + numeroObstacle);
+    } else if ((obstaclesLeft > 0) && (dejaPasseParLa==true)) {
+        dejaPasseParLa = false;
+    }
+    
+    
+}
+function afficherGameOver() {
+    var titleOver = "Game Over";
 
-            document.querySelector("#titreOver").innerText = titleOver;   
-            document.querySelector("#score-over").innerText = " Score : " + score;
-            obstacleArray[randomNumber].style.animation = "none";
+    document.querySelector("#titreOver").innerText = titleOver;   
+    document.querySelector("#score-over").innerText = " Score : " + score;
+    obstacleArray[typeObstacle].style.animation = "none";
 
 
-            pause = false;
-        
-            document.getElementById('titreOver').style.display = 'block'; 
-            document.getElementById('button-pause').style.display = 'none';    
-            document.getElementById('score').style.display = 'none';  
-            document.getElementById('niveau').style.display = 'none';  
-            document.getElementById('button-recommencer').style.display = 'block';  
-            document.getElementById('button-quitter-over').style.display = 'block';  
-            document.getElementById('score-pause').style.display = 'none';  
-            document.getElementById('personnage').style.display = 'none'; 
-            document.getElementById('obstacles').style.display = 'none'; 
+    pause = false;
 
-            clearInterval(interval);
-            
+    document.getElementById('titreOver').style.display = 'block'; 
+    document.getElementById('button-pause').style.display = 'none';    
+    document.getElementById('score').style.display = 'none';  
+    document.getElementById('niveau').style.display = 'none';  
+    document.getElementById('button-recommencer').style.display = 'block';  
+    document.getElementById('button-quitter-over').style.display = 'block';  
+    document.getElementById('score-pause').style.display = 'none';  
+    document.getElementById('personnage').style.display = 'none'; 
+    document.getElementById('obstacles').style.display = 'none'; 
+
+    clearInterval(verification);
+}
+function isEmptyObject(value) {
+    return Object.keys(value).length === 0 && value.constructor === Object;
+}
+function determinerTypeObstacle(numeroObstacle) {
+    
+    if (listeDesObstacles != undefined) {
+        nomObstacle = listeDesObstacles[numeroObstacle];
+        //console.log("Nom de l'obstacle:"+nomObstacle);
+        switch (nomObstacle) {
+            case 'oiseau':
+                typeObstacle = 1;
+                break;
+            case 'cactus':
+                typeObstacle = 0;
+                break;
+            default:
+                afficherGameOver();
+                break;
         }
-
-
-        else if (obstaclesLeft  < 0) { // si l'obstacle n'est pas entré en collision avec le personnage
-            obstacleArray[randomNumber].style.display = "none"; // on cache l'obstacle
-            randomNumber = Math.floor(Math.random() * obstacleArray.length); // on génère un nombre aléatoire
-            console.log(randomNumber, "reinit"); // console log
-            obstacleArray[randomNumber].style.display = "block"; // nouvel obstacle selon le nombre aléatoire
-        }
-    },1)
+    } else {
+        typeObstacle = Math.floor(Math.random() * obstacleArray.length);
+    } 
+    
+    return typeObstacle;
 }
 
 /* -------------- AFFICHE LE SCORE PENDANT LE JEU --------------- */
@@ -101,17 +141,17 @@ function pauseJeux(){
     document.getElementById('obstacles4').style.display = 'none'
     document.getElementById('personnage').style.display = 'none'
 
-        document.getElementById('obstacles').style.opacity = "0";
-        document.getElementById('obstacles').style.animationPlayState = "paused";
+    document.getElementById('obstacles').style.opacity = "0";
+    document.getElementById('obstacles').style.animationPlayState = "paused";
 
-        document.getElementById('obstacles2').style.opacity = "0";
-        document.getElementById('obstacles2').style.animationPlayState = "paused";
+    document.getElementById('obstacles2').style.opacity = "0";
+    document.getElementById('obstacles2').style.animationPlayState = "paused";
 
-        document.getElementById('obstacles3').style.opacity = "0";
-        document.getElementById('obstacles3').style.animationPlayState = "paused";
+    document.getElementById('obstacles3').style.opacity = "0";
+    document.getElementById('obstacles3').style.animationPlayState = "paused";
 
-        document.getElementById('obstacles4').style.opacity = "0";
-        document.getElementById('obstacles4').style.animationPlayState = "paused";
+    document.getElementById('obstacles4').style.opacity = "0";
+    document.getElementById('obstacles4').style.animationPlayState = "paused";
 
 
 }
@@ -128,17 +168,17 @@ function repriseJeux(){
     document.getElementById('score-pause').style.display = 'none';
     document.getElementById('personnage').style.display = 'block'
 
-        document.getElementById('obstacles').style.opacity = "1";
-        document.getElementById('obstacles').style.animationPlayState = "running";
+    document.getElementById('obstacles').style.opacity = "1";
+    document.getElementById('obstacles').style.animationPlayState = "running";
 
-        document.getElementById('obstacles2').style.opacity = "1";
-        document.getElementById('obstacles2').style.animationPlayState = "running";
+    document.getElementById('obstacles2').style.opacity = "1";
+    document.getElementById('obstacles2').style.animationPlayState = "running";
 
-        document.getElementById('obstacles3').style.opacity = "1";
-        document.getElementById('obstacles3').style.animationPlayState = "running";
+    document.getElementById('obstacles3').style.opacity = "1";
+    document.getElementById('obstacles3').style.animationPlayState = "running";
 
-        document.getElementById('obstacles4').style.opacity = "1";
-        document.getElementById('obstacles4').style.animationPlayState = "running";
+    document.getElementById('obstacles4').style.opacity = "1";
+    document.getElementById('obstacles4').style.animationPlayState = "running";
 
 
     interval = setInterval(afficherScore, 100);
@@ -165,7 +205,7 @@ function sauter(){
 
 //fonction pour déterminer la key à presser
 document.addEventListener("keydown",function(e){
-    if(e.keyCode==65){
+    if(e.key == 'a'){
         sauter()
     }
 });
@@ -173,10 +213,10 @@ document.addEventListener("keydown",function(e){
 //afficher le perso accroupi et fait disparaitre le luffy normal
 
 document.addEventListener("keydown",function(e){
-    if(e.keyCode==80){
+    if(e.key=='q'){
         perso.style.bottom = "40px"
         accroupi.style.display = "unset"
-        console.log ("persoBottom")
+        //console.log ("persoBottom")
         console.log(parseInt(window.getComputedStyle(perso).getPropertyValue("bottom")))
         perso.style.display = "none"
     }
@@ -184,13 +224,15 @@ document.addEventListener("keydown",function(e){
 
 //afficher le perso normal et disparaitre luffy accroupi
 document.addEventListener("keyup",function(e){
-    if(e.keyCode==80){
+    if(e.key=='q'){
         perso.style.bottom = "50px"
         accroupi.style.display = "none"
-        console.log ("persoBottom")
+        //console.log ("persoBottom")
         perso.style.display = "unset"
     }
 });
 
-
+var listeDesObstaclesEnJson = sessionStorage.getItem('listeDesObstacles');
+listeDesObstacles = JSON.parse(listeDesObstaclesEnJson);
+console.log(listeDesObstacles);
 obstacleHandler();
---
+++ scriptEdition.js
@@ -0,0 +1,296 @@
+//Définition de la map tabSave (contient les clé, valeur des obstacles)
+var tabSave = new Map();
+//Définition de la variable obstacle (permet de savoir a quel obstacle nous en somme)
+var obstacle = 0
+//fonction qui modifie la value si on clique dessus
+function newValue(pId){
+    console.log(pId)
+    //On récupère la div contenant le p
+    divId = document.getElementById(pId)
+    //Modification de la valeur de value (on lui donne une nouvelle valeur)
+    value = prompt("Obstacle "+pId+": a ou b (a pour cactus et b pour oiseau)")
+    if(value==="a"){
+        value = "cactus"
+    } else if(value==="b"){
+        value = "oiseau"
+    } else {
+        alert("valeur incorrecte, veuillez taper a ou b sinon l'obstacle a sera mis par défaut")
+        value = prompt("Obstacle a ou b")
+        if(value==="a"){
+            value = "cactus"
+        } else if(value==="b"){
+            value = "oiseau"
+        } else {
+            value = "cactus"
+            console.log(value)
+        }
+    }
+    //On récupère l'id du p a supprimer
+    var pSupp = document.getElementById("p"+pId)
+    //on supprime le p
+    pSupp.remove()
+    //on crée un nouveau p avec la nouvelle valeur (voir fonction newObstacles)
+    let nP = document.createElement('p')
+    nP.id = "p"+pId
+    nP.classList.add("pob");
+    var contenu = document.createTextNode('Obstacles '+ pId+": "+value)
+    nP.addEventListener('click', function() {
+        newValue(pId);
+    });
+    //On ajoute le contenu dans le p
+    nP.appendChild(contenu)
+    //On ajoute le p dans la div
+    divId.appendChild(nP)
+    //On supprime l'ancienne clé dans tabSave
+    tabSave.delete(pId)
+    //On ajoute une nouvelle combinaison clé valeur
+    pId=parseInt(pId)
+    tabSave.set(pId, value)
+}
+
+// fonction qui crée un nouvel obstacle, enregistre sa valeur et la transmet à tabSave
+function newObstacle(){
+    obstacle++
+    //On defini une variable qui va contenir la valeur de l'obstacle
+    var value = prompt("Obstacle "+obstacle+": a ou b (a pour cactus et b pour oiseau)")
+    if(value==="a"){
+        value = "cactus"
+    } else if(value==="b"){
+        value = "oiseau"
+    } else {
+        alert("valeur incorrecte, veuillez taper a ou b sinon l'obstacle a sera mis par défaut")
+        value = prompt("Obstacle a ou b")
+        if(value==="a"){
+            value = "cactus"
+        } else if(value==="b"){
+            value = "oiseau"
+        } else {
+            value = "cactus"
+            console.log(value)
+        }
+    }
+    //On sauvegarde la value obtenue dans tabSave [clé=nombre obstacle, valeur=value]
+    tabSave.set(obstacle, value)
+    console.log(tabSave)
+    // Sélectionne la section id secP
+    let section = document.querySelector("#secP")
+    // On crée une div
+    var nDiv = document.createElement('div');
+    //On donne une id à la div
+    console.log("Id de l'obstacle : ",obstacle)
+    nDiv.id = obstacle
+    console.log("Id de la div : ",nDiv.id)
+    // On donne une class a la div (css)
+    nDiv.classList.add("divcss");
+    // Ici on crée un p
+    var nP = document.createElement('p')
+    // Ici on donne une id au p
+    nP.id = "p"+obstacle
+    // On donne une class au p (css)
+    nP.classList.add("pob");
+    // On rajoute une intéraction click au boutons
+    nP.addEventListener('click', function() {
+        newValue(nDiv.id);
+    });
+    //On crée un contenu
+    var contenu = document.createTextNode('Obstacles '+ obstacle+": "+value)
+    //On ajoute le contenu dans le p
+    nP.appendChild(contenu)
+    //On ajoute le p dans la div
+    nDiv.appendChild(nP)
+    //On met le tout dans la section
+    section.appendChild(nDiv)
+}
+//Fonction qui supprime le dernier obstacle enregistré
+function lessObstacle(){
+    let section = document.querySelector("#secP")
+    var divSupp = document.getElementById(obstacle)
+    divSupp.remove()
+    tabSave.delete(obstacle)
+    obstacle--
+}
+//Fonction qui crée des boutons dans le section
+function newButtons(){
+    // Sélectionne la section id secP
+    let section = document.querySelector("#secP")
+    // On crée les deux boutons et on leur donne une id
+    var buttonPlus = document.createElement('button')
+    var buttonMoins = document.createElement('button')
+    buttonPlus.id = "buttonPlus"
+    buttonMoins.id = "buttonMoins"
+    // On ajoute du texte au bouton
+    buttonPlus.innerHTML = 'Cliquez ici pour rajouter un obstacle';
+    buttonMoins.innerHTML = 'Cliquez ici pour supprimez le dernier obstacle';
+    // On ajoute une interaction au bouton
+    buttonPlus.addEventListener('click', allButtons);
+    buttonMoins.addEventListener('click', lessObstacle);
+    // On ajoute nos boutons à la section
+    section.appendChild(buttonPlus)
+    section.appendChild(buttonMoins)
+}
+//Fonction qui gère la coordination des fonctions (première fonction appellé par le code)
+function edition(){
+    // Appel de la fonction newObstacle
+    newObstacle();
+    // Suppresion du premier bouton
+    //var supp = document.getElementById("firstButton");
+    //supp.remove();
+    // Appel de la fonction newButton
+    //newButtons();
+}
+//Fonction qui supprime les deux boutons
+function suppTwoButtons(){
+    let first = document.getElementById("buttonPlus")
+    let second = document.getElementById("buttonMoins")
+    first.remove()
+    second.remove()
+}
+// Fonction qui supprime les boutons et les recrées en bas de page + Crée le nouvel obstacle
+function allButtons(){
+    suppTwoButtons()
+    newObstacle()
+    newButtons()
+}
+// Fonction qui convertis tabSave en format Json
+function convertToJSON() {
+    const json = {};
+    for (const [key, value] of tabSave) {
+      json[key] = value;
+    }
+    return json;
+  }
+// Fonction qui convertis le fichier Json en stringify
+function string(){
+    // Récupération de convertToJSON et mise en variable
+    var result = convertToJSON()
+    // Passage en string de la var result
+    var lol = JSON.stringify(result)
+    // Renvoie de lol
+    return lol
+
+    // console.log(Object.values(result)) // Afficher seulement les valeurs 
+    // var dgh = JSON.parse(JSON.stringify(result)); // == De Json à Js
+}
+// Fonction qui permet de télécharger un fichier text contenant tabSave en Json
+function createDownloadableFile() {
+    let content = string();
+    const blob = new Blob([content], { type: "text/plain" });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement("a");
+    link.download = "obstacles.txt";
+    link.href = url;
+    link.click();
+  }
+// Fonction qui permet de récupérer les données à partir d'un fichier txt
+var data = ""
+function importFile(file) {
+    var reader = new FileReader();
+    reader.readAsText(file);
+    reader.onload = function(event) {
+      // Récupération du contenu du fichier
+      var content = event.target.result;
+      sessionStorage.setItem('listeDesObstacles', content);
+      // Conversion du contenu en objet
+      data = JSON.parse(content);
+      // Affichage de la var data
+      console.log(data);
+      // Renvoie de data
+      Affichage()
+      
+      return data
+      // console.log(Object.values(data))
+    }
+  }
+// Fonction qui affiche les obstacles après lecture du fichier
+var parcourir = 1
+function Affichage(){
+    valeurArray = data[parcourir]
+    const taille = Object.keys(data).length;
+    console.log(taille)
+    // console.log("1 : ", data, parcourir)
+    // console.log(valeurArray)
+    // console.log("longueur ", tabSave.length)
+    // console.log(data)
+    // On crée l'obstacle 
+        while(parcourir < taille+1){
+            valeurArray = data[parcourir]
+            console.log("log de valeur array", valeurArray)
+            // Sélectionne la section id secP
+            let section = document.querySelector("#secP")
+            // On crée une div
+            var nDiv = document.createElement('div');
+            //On donne une id à la div
+            console.log("Id de l'obstacle : ",parcourir)
+            nDiv.id = parcourir
+            console.log("Id de la div : ",nDiv.id)
+            // On donne une class a la div (css)
+            nDiv.classList.add("divcss");
+            // Ici on crée un p
+            var nP = document.createElement('p')
+            // Ici on donne une id au p
+            nP.id = "p"+parcourir
+            // On donne une class au p (css)
+            nP.classList.add("pob");
+            //On crée un contenu
+            var contenu = document.createTextNode('Obstacles '+ parcourir+": "+valeurArray)
+            //On ajoute le contenu dans le p
+            nP.appendChild(contenu)
+            //On ajoute le p dans la div
+            nDiv.appendChild(nP)
+            //On met le tout dans la section
+            section.appendChild(nDiv)
+            //on augmente parcourir
+            parcourir++
+            console.log("valeur de : ",parcourir)
+        }
+}
+
+// Fonction de gestion des obstacles en fonction du charger 
+var perso = document.querySelector("#personnage");
+var obstacles = document.querySelector("#obstacles");
+var obstacles2 = document.querySelector("#obstacles2");
+var obstacles3 = document.querySelector("#obstacles3");
+var obstacles4 = document.querySelector("#obstacles4");
+var accroupi = document.querySelector("#accroupi");
+//liste des obstacles
+var obstacleArray = [obstacles, obstacles2, obstacles3, obstacles4]
+var parcou = 0
+// Fonction pour déterminer quel obstacle va spawn
+function obstacleCharger(){
+    const taille2 = Object.keys(data).length;
+    while (parcou < taille2+1){
+        valeurArray = data[parcou]
+        var randomNumber = data
+        if (randomNumber==="cactus"){
+            randomNumber=1
+        } else {
+            randomNumber=2
+        }
+        console.log("a", randomNumber)
+        console.log(randomNumber);
+        obstacleArray[randomNumber].style.display = "block";
+
+        //verification que l'objet est passé ou qu'il y a eu contact
+        var verification = setInterval(function(){
+            var persoTop = parseInt(window.getComputedStyle(perso).getPropertyValue("top")) ;
+            var obstaclesLeft = parseInt(window.getComputedStyle(obstacleArray[randomNumber]).getPropertyValue("left"));
+            var obstaclesTop = parseInt(window.getComputedStyle(obstacleArray[randomNumber]).getPropertyValue("top"));
+
+            if(obstaclesLeft<20 && obstaclesLeft >0 && (persoTop> 472 && randomNumber!=1 || persoTop<600 && randomNumber==1)){
+                console.log(persoTop)
+                obstacleArray[randomNumber].style.animation = "none";
+                alert("Loose")
+            }
+
+            else if (obstaclesLeft  < 0) { // si l'obstacle n'est pas entré en collision avec le personnage
+                obstacleArray[randomNumber].style.display = "none"; // on cache l'obstacle
+                randomNumber = Math.floor(Math.random() * obstacleArray.length); // on génère un nombre aléatoire
+                console.log(randomNumber, "reinit");
+                obstacleArray[randomNumber].style.display = "block"; // nouvel obstacle selon le nombre aléatoire
+            }
+        },1)
+    }
+}
+function plusPlus(){
+    
+}
